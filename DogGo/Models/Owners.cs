5fdf076673f9fd5652f087441a3c2868056d5e82
Nelson Taylor Lumpkin <ossocasa@gmail.com>
2023-03-29 13:29:43 -0500

    first commit

---
+++ DogGo/Models/Owners.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DogGo.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NeighborhoodId { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }    
+        public string Email { get; set; }
+        public string Neighborhood { get; set; }
+        public List<Dog> Dogs { get; set; } = new List<Dog>();
+    }
+
+}
+
+
---
+++ DogGo/Models/Walker.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DogGo.Models
+{
+    public class Walker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NeighborhoodId { get; set; }
+        public string ImageUrl { get; set; }
+        public Neighborhood Neighborhood { get; set; }
+    }
+   
+}
